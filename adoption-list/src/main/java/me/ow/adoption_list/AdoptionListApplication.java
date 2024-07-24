e95e5178b6fcfd877f95934c797cb60c4553fbb0
Mariana Castro <fercastro.mariana@gmail.com>
2024-07-24 01:37:07 -0300

    first commit

---
+++ adoption-list/src/main/java/me/ow/adoption_list/AdoptionListApplication.java
@@ -0,0 +1,13 @@
+package me.ow.adoption_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptionListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptionListApplication.class, args);
+	}
+
+}
---
+++ adoption-list/src/test/java/me/ow/adoption_list/AdoptionListApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ow.adoption_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
